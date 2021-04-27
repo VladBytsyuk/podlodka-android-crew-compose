15947545f841a0eb3340f3c2831e1f3e8f7ffc11
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-04-27 18:59:11 +0300

    Added compose

--- app/src/main/java/com/vbytsyuk/paccomposeapp/MainActivity.kt
+++ app/src/main/java/com/vbytsyuk/paccomposeapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.vbytsyuk.paccomposeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent { Content() }
     }
+}
+
+@Preview
+@Composable
+private fun Content() {
+    Text("Hello World!")
 }
