eb6b537cdf5a2d683034f89b25551857bba5f598
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-04-27 20:19:19 +0300

    Added sessions section into main screen

--- app/src/main/java/com/vbytsyuk/paccomposeapp/MainActivity.kt
+++ app/src/main/java/com/vbytsyuk/paccomposeapp/MainActivity.kt
@@ -3,9 +3,12 @@ package com.vbytsyuk.paccomposeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.vbytsyuk.paccomposeapp.views.SessionCard
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,11 @@ class MainActivity : AppCompatActivity() {
 
 @Preview
 @Composable
-fun Content() {
-    Text("Hello World!")
+fun Content() = Column {
+    Text("Sessions")
+    LazyColumn() {
+        MockSessions.forEach { 
+            item { SessionCard(session = it) }
+        }
+    }
 }
